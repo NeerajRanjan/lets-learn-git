6c7ae3dc3fc4ee82876f648574abd2f4b1b82f17
NeerajRanjan <rajneeraj8521@gmail.com>
2022-04-09 17:32:33 +0530

    added multiply.c

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[]) {
+  int a , b;
+  printf("enter first number/n");
+  scanf("%d",&a);
+  printf("enter second number/n");
+  scanf("%d",&b);
+  printf("%d\n",a*b);
+  return 0;
+}
